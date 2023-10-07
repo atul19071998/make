ebcfb1f20356680ec22b130d2270244018431cff
Atul <ajayc5314@gmail.com>
2023-10-07 11:43:50 +0530

    files upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const thumbnails = document.querySelectorAll('.thumbnail');
+const displayedImage = document.getElementById('displayed-image');
+
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', () => {
+        // Set the displayed image source to the clicked thumbnail's source
+        displayedImage.src = thumbnail.src;
+        displayedImage.alt = thumbnail.alt;
+    });
+});
